619d02648c8207febf7ea69e7ee92c40e178aed6
Lhaniii <ilhan95210@icloud.com>
2024-05-24 08:42:17 +0000

    task 2 complete

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class for cheking object"""
+
+
+def is_same_class(obj, a_class):
+    """check object"""
+    if type(obj) == a_class:
+        return True
+    return False
