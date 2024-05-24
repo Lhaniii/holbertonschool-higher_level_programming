618753142b905bac8447c9f89fa3db6a2eaa389f
Lhaniii <ilhan95210@icloud.com>
2024-05-24 09:43:02 +0000

    task 3 complete

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/python3
+"""Define a class for inherited cheking object"""
+
+def is_kind_of_class(obj, a_class):
+    """check object is an instance or inherited instance"""
+    if isinstance(obj, a_class):
+        return True
+    return False
