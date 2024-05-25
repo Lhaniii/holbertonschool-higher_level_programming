69ccd1f9b647980a1bf9535db3b0ddc23089aeee
Lhaniii <ilhan95210@icloud.com>
2024-05-25 20:30:46 +0000

    task 0 complete

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Define classe of animal """
+
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """define class animal"""
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    """define class Dog based of class Animal"""
+    def sound(self):
+        return "bark"
+
+class Cat(Animal):
+    """define class cat based of class animal"""
+    def sound(self):
+        return "Meow"
