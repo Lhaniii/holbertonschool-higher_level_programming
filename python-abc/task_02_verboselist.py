91f6954360eecb2a568be550c78f72f5d6f7ee78
Lhaniii <ilhan95210@icloud.com>
2024-05-26 00:28:30 +0000

    Task 2 test

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+class VerboseList(list):
+    """define Verboselist"""
+    def append(self, item):
+        """Append item in the list and print message"""
+        super().append(item)
+        print("added [{}] to the list.".format(item))
+
+    def extend(self, item):
+        """extends list with given item and print message."""
+        super().extend(item)
+        print("Extended the list with [{}] items.".format(len(item)))
+
+    def remove(self, item):
+        """Remove first occurence of item"""
+        print("Removed [{}] form the list.".format(item))
+        super().remove(item)
+
+    def pop(self, index=-1):
+        item = super().pop(index)
+        """removes and returns item specifi and print a message"""
+        print("Popped [{}] from the list.".format(item))
+        return item
