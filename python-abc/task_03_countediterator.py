bd7133278fb454366ed204bdcc554bda66a556ab
Lhaniii <ilhan95210@icloud.com>
2024-05-26 12:23:47 +0000

    task 3 complete

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Define class CountedIterator"""
+
+class CountedIterator:
+    """Define class countedIterator"""
+    def __init__(self, some_iterable):
+        self.iterator = iter(some_iterable)
+        self.counter = 0
+
+    def get_count(self):
+        """Return the count"""
+        return self.counter
+
+    def __next__(self):
+        """Override the next"""
+        try:
+            i = next(self.iterator)
+            self.counter =+ 1
+            return i 
+        except StopIteration:
+            raise StopIteration
