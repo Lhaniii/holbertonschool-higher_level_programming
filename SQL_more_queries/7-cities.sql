8ba5d2901e60254ce1480f1b28b6629fce315ee0
Lhaniii <ilhan95210@icloud.com>
2024-06-21 01:04:05 +0200

    task 7 complete

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS cities
 (
     id INT NOT NULL AUTO_INCREMENTPRIMARY KEY,
     state_id INT NOT NULL, name VARCHAR(256) NOT NULL,
-    UNIQUE (id), FOREIGN KEY(state_id) REFERENCES states(id)
+    UNIQUE (id),
+    FOREIGN KEY(state_id) REFERENCES states(id)
 );
