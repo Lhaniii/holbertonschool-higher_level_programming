32baf1e5d4434994e869a49c14d7459e039c86c8
Lhaniii <ilhan95210@icloud.com>
2024-06-21 01:17:46 +0200

    task 8 complete

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+--  A script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL, name VARCHAR(256) NOT NULL,
+    UNIQUE (id), FOREIGN KEY(state_id) REFERENCES state(id)
+);
