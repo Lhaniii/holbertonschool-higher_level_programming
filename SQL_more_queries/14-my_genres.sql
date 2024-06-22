ce882ba3f369723ad8d1507868438de995c8ff31
Lhaniii <ilhan95210@icloud.com>
2024-06-22 15:09:29 +0200

    task 14 complete

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- a script that uses the hbtn_0d_tvshows.
-SELECT tv_genres.nam FROM tv_genres
+SELECT tv_genres.name FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_show ON tv_show_genres.show_id = tv_shows.id
 WHERE tv.shows.title = "Dexter"
